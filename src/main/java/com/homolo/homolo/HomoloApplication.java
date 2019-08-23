961f92cb3a0d67d3fa624224f2db0955638f2a12
李帅领 <lishuailing@homolo.com>
2019-08-23 16:55:04 +0800

    first

---
+++ src/main/java/com/homolo/homolo/HomoloApplication.java
@@ -0,0 +1,13 @@
+package com.homolo.homolo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomoloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomoloApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/homolo/homolo/entity/User.java
@@ -0,0 +1,50 @@
+package com.homolo.homolo.entity;
+
+/**
+ * @Author homolo
+ * @DESC
+ * @Create 2019-08-23  下午12:18
+ */
+public class User {
+
+	private Integer id;
+	private String name;
+	private Integer age;
+
+	public User(String name, Integer age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	public User() {
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "User{" + "name='" + name + '\'' + ", age=" + age + '}';
+	}
+}

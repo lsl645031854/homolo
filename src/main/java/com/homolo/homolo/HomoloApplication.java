4b35d138a001210b83bfbd82be9e66202691dcc2
朱辉 <zhuhui@homolo.com>
2019-08-26 15:04:34 +0800

    线程次配置

--- src/main/java/com/homolo/homolo/HomoloApplication.java
+++ src/main/java/com/homolo/homolo/HomoloApplication.java
@@ -2,8 +2,10 @@ package com.homolo.homolo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class HomoloApplication {
 
 	public static void main(String[] args) {
